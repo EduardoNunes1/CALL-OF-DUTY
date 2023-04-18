87a08a434463f7a3b49d4cfc3ecf064a496e7f32
Eduardo Nunes <eduardonunes8349@gmail.com>
2023-04-18 08:42:57 -0300

    Atualizando a organização das informações dos personagens.

--- CALL-OF-DUTY/Models/Personagens.cs
+++ CALL-OF-DUTY/Models/Personagens.cs
@@ -2,11 +2,11 @@ namespace CALL_OF_DUTY.Models
 {
     public class Personagens
     {
+        public int Numero { get; set; }
         public string Nome { get; set; }
         public string Descrição { get; set; }
-        public string Patente { get; set; }
         public string Imagem { get; set; }
+        public string Patente { get; set; }
         public List<string> Tipo { get; set; }
-        public int Numero { get; set; }
     }
 }
